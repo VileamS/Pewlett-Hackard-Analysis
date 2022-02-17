aefc5717772bc336f73d1c54b2e084ff2c094edd
Vileam Siv <svileam@gmail.com>
2022-02-17 00:25:51 -0600

    Finished analysis of Module 7 challenge

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,32 @@
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN title as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+SELECT * FROM retirement_titles
+
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+
+INTO unique_titles
+FROM retirement_titles
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, title DESC;
+SELECT * FROM unique_titles
+
+SELECT COUNT(ut.emp_no),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+SELECT * FROM retiring_titles
